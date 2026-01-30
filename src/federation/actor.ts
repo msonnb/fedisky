a9f1c72fdd41c75af7ba9c8bec43161f5f46d4b1
Martin Sonnberger <martin@sonnberger.email>
2026-01-30 15:58:05 +0100

    use percent-encoded DID in alsoKnownAs

--- src/federation/actor.ts
+++ src/federation/actor.ts
@@ -63,7 +63,7 @@ export function setupActorDispatcher(ctx: AppContext) {
         )
         return new Person({
           id: fedCtx.getActorUri(identifier),
-          alias: new URL(`at://${account.handle}`),
+          alias: new URL(`at://${encodeURIComponent(account.did)}`),
           name: profile?.displayName,
           summary: profile?.description,
           preferredUsername: account.handle.split('.').at(0),
